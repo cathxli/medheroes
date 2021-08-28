ff92b51c9d10d0351c80d746caa7fcd7c8c92aa6
cathxli <catherine_li1@brown.edu>
2021-08-28 00:44:55 -0700

    edit port info for heroku deployment

--- server.js
+++ server.js
@@ -62,8 +62,8 @@ app.get('/express_backend', (req, res) => {
     res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
 
-app.listen(5000, function(){
-    console.log('- Server listening on port 5000');
+app.listen(port, function(){
+    console.log('Server listening on port ', port);
 });
 
 //////////////////////// DEFINE SCHEMAS ///////////////////////
